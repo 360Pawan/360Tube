a9e9c3226f25ee95ce9a5bc1ea519f31dc353dd3
360pawan <360pawan@gmail.com>
2024-01-20 14:15:30 +0530

    updated like route to TS

--- src/controllers/like.controller.ts
+++ src/controllers/like.controller.ts
@@ -1,221 +1,238 @@
+import { Request, Response } from "express";
 import mongoose, { isValidObjectId } from "mongoose";
 
-import { Like } from "../models/like.model.js";
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { Video } from "../models/video.model.js";
-import { Comment } from "../models/comment.model.js";
-import { Tweet } from "../models/tweet.model.js";
+import { Like } from "@/models/like.model";
+import { ApiError } from "@/utils/ApiError";
+import { ApiResponse } from "@/utils/ApiResponse";
+import { asyncHandler } from "@/utils/asyncHandler";
+import { Video } from "@/models/video.model";
+import { Comment } from "@/models/comment.model";
+import { Tweet } from "@/models/tweet.model";
 
-const toggleVideoLike = asyncHandler(async (request, response) => {
-  const { videoId } = request.params;
+const toggleVideoLike = asyncHandler(
+  async (request: Request, response: Response) => {
+    const { videoId } = request.params;
 
-  if (!videoId || !isValidObjectId(videoId)) {
-    return response
-      .status(400)
-      .json(new ApiError(400, "😰 Not a valid video id."));
-  }
-
-  const video = await Video.findById(videoId);
-
-  if (!video) {
-    return response.status(404).json(new ApiError(404, "😰 No video found"));
-  }
-
-  const likedVideo = await Like.findOne({
-    $and: [{ video: videoId }, { likedBy: request.user._id }],
-  });
-
-  if (!likedVideo) {
-    const like = await Like.create({
-      video: videoId,
-      likedBy: request.user._id,
-    });
-
-    if (!like) {
+    if (!videoId || !isValidObjectId(videoId)) {
       return response
-        .status(500)
-        .json(new ApiError(500, "😰 Error while like."));
+        .status(400)
+        .json(new ApiError(400, "😰 Not a valid video id."));
     }
 
-    return response
-      .status(200)
-      .json(new ApiResponse(200, {}, "👍 Video liked successfully."));
-  } else {
-    const removeLike = await Like.deleteOne({
-      _id: likedVideo._id,
-    });
+    const video = await Video.findById(videoId);
 
-    if (removeLike.deletedCount === 0) {
-      return response
-        .status(500)
-        .json(new ApiError(500, "😰 Error while like."));
+    if (!video) {
+      return response.status(404).json(new ApiError(404, "😰 No video found"));
     }
 
-    return response
-      .status(200)
-      .json(new ApiResponse(200, {}, "👍 Video like removed successfully."));
-  }
-});
-
-const toggleCommentLike = asyncHandler(async (request, response) => {
-  const { commentId } = request.params;
-
-  if (!commentId || !isValidObjectId(commentId)) {
-    return response
-      .status(400)
-      .json(new ApiError(400, "😰 Not a valid video id."));
-  }
-
-  const comment = await Comment.findById(commentId);
-
-  if (!comment) {
-    return response.status(404).json(new ApiError(404, "😰 No comment found"));
-  }
-
-  const likedComment = await Like.findOne({
-    $and: [{ comment: commentId }, { likedBy: request.user._id }],
-  });
-
-  if (!likedComment) {
-    const like = await Like.create({
-      comment: commentId,
-      likedBy: request.user._id,
+    const likedVideo = await Like.findOne({
+      $and: [{ video: videoId }, { likedBy: request.user._id }],
     });
 
-    if (!like) {
+    if (!likedVideo) {
+      const like = await Like.create({
+        video: videoId,
+        likedBy: request.user._id,
+      });
+
+      if (!like) {
+        return response
+          .status(500)
+          .json(new ApiError(500, "😰 Error while like."));
+      }
+
       return response
-        .status(500)
-        .json(new ApiError(500, "😰 Error while like."));
+        .status(200)
+        .json(new ApiResponse(200, {}, "👍 Video liked successfully."));
+    } else {
+      const removeLike = await Like.deleteOne({
+        _id: likedVideo._id,
+      });
+
+      if (removeLike.deletedCount === 0) {
+        return response
+          .status(500)
+          .json(new ApiError(500, "😰 Error while like."));
+      }
+
+      return response
+        .status(200)
+        .json(new ApiResponse(200, {}, "👍 Video like removed successfully."));
+    }
+  }
+);
+
+const toggleCommentLike = asyncHandler(
+  async (request: Request, response: Response) => {
+    const { commentId } = request.params;
+
+    if (!commentId || !isValidObjectId(commentId)) {
+      return response
+        .status(400)
+        .json(new ApiError(400, "😰 Not a valid video id."));
     }
 
-    return response
-      .status(200)
-      .json(new ApiResponse(200, {}, "👍 Comment liked successfully."));
-  } else {
-    const removeLike = await Like.deleteOne({
-      _id: likedComment._id,
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      return response
+        .status(404)
+        .json(new ApiError(404, "😰 No comment found"));
+    }
+
+    const likedComment = await Like.findOne({
+      $and: [{ comment: commentId }, { likedBy: request.user._id }],
     });
 
-    if (removeLike.deletedCount === 0) {
+    if (!likedComment) {
+      const like = await Like.create({
+        comment: commentId,
+        likedBy: request.user._id,
+      });
+
+      if (!like) {
+        return response
+          .status(500)
+          .json(new ApiError(500, "😰 Error while like."));
+      }
+
       return response
-        .status(500)
-        .json(new ApiError(500, "😰 Error while like."));
+        .status(200)
+        .json(new ApiResponse(200, {}, "👍 Comment liked successfully."));
+    } else {
+      const removeLike = await Like.deleteOne({
+        _id: likedComment._id,
+      });
+
+      if (removeLike.deletedCount === 0) {
+        return response
+          .status(500)
+          .json(new ApiError(500, "😰 Error while like."));
+      }
+
+      return response
+        .status(200)
+        .json(
+          new ApiResponse(200, {}, "👍 Comment like removed successfully.")
+        );
+    }
+  }
+);
+
+const toggleTweetLike = asyncHandler(
+  async (request: Request, response: Response) => {
+    const { tweetId } = request.params;
+
+    if (!tweetId || !isValidObjectId(tweetId)) {
+      return response
+        .status(400)
+        .json(new ApiError(400, "😰 Not a valid tweet id."));
     }
 
-    return response
-      .status(200)
-      .json(new ApiResponse(200, {}, "👍 Comment like removed successfully."));
-  }
-});
+    const tweet = await Tweet.findById(tweetId);
 
-const toggleTweetLike = asyncHandler(async (request, response) => {
-  const { tweetId } = request.params;
+    if (!tweet) {
+      return response.status(404).json(new ApiError(404, "😰 No tweet found"));
+    }
 
-  if (!tweetId || !isValidObjectId(tweetId)) {
-    return response
-      .status(400)
-      .json(new ApiError(400, "😰 Not a valid tweet id."));
-  }
-
-  const tweet = await Tweet.findById(tweetId);
-
-  if (!tweet) {
-    return response.status(404).json(new ApiError(404, "😰 No tweet found"));
-  }
-
-  const likedTweet = await Like.findOne({
-    $and: [{ tweet: tweetId }, { likedBy: request.user._id }],
-  });
-
-  if (!likedTweet) {
-    const like = await Like.create({
-      tweet: tweetId,
-      likedBy: request.user._id,
+    const likedTweet = await Like.findOne({
+      $and: [{ tweet: tweetId }, { likedBy: request.user._id }],
     });
 
-    if (!like) {
+    if (!likedTweet) {
+      const like = await Like.create({
+        tweet: tweetId,
+        likedBy: request.user._id,
+      });
+
+      if (!like) {
+        return response
+          .status(500)
+          .json(new ApiError(500, "😰 Error while like."));
+      }
+
       return response
-        .status(500)
-        .json(new ApiError(500, "😰 Error while like."));
-    }
+        .status(200)
+        .json(new ApiResponse(200, {}, "👍 Tweet liked successfully."));
+    } else {
+      const removeLike = await Like.deleteOne({
+        _id: likedTweet._id,
+      });
 
-    return response
-      .status(200)
-      .json(new ApiResponse(200, {}, "👍 Tweet liked successfully."));
-  } else {
-    const removeLike = await Like.deleteOne({
-      _id: likedTweet._id,
-    });
+      if (removeLike.deletedCount === 0) {
+        return response
+          .status(500)
+          .json(new ApiError(500, "😰 Error while like."));
+      }
 
-    if (removeLike.deletedCount === 0) {
       return response
-        .status(500)
-        .json(new ApiError(500, "😰 Error while like."));
+        .status(200)
+        .json(new ApiResponse(200, {}, "👍 Tweet like removed successfully."));
     }
-
-    return response
-      .status(200)
-      .json(new ApiResponse(200, {}, "👍 Tweet like removed successfully."));
   }
-});
+);
 
-const getLikedVideos = asyncHandler(async (request, response) => {
-  const likedVideos = await Like.aggregate([
-    {
-      $match: {
-        $and: [
-          { likedBy: new mongoose.Types.ObjectId(request.user._id) },
-          { video: { $exists: true } },
-        ],
+const getLikedVideos = asyncHandler(
+  async (request: Request, response: Response) => {
+    const likedVideos = await Like.aggregate([
+      {
+        $match: {
+          $and: [
+            { likedBy: new mongoose.Types.ObjectId(request.user._id) },
+            { video: { $exists: true } },
+          ],
+        },
       },
-    },
-    {
-      $lookup: {
-        from: "videos",
-        localField: "video",
-        foreignField: "_id",
-        as: "video",
-        pipeline: [
-          {
-            $lookup: {
-              from: "users",
-              localField: "owner",
-              foreignField: "_id",
-              as: "owner",
-              pipeline: [
-                {
-                  $project: {
-                    fullName: 1,
-                    username: 1,
-                    avatar: 1,
+      {
+        $lookup: {
+          from: "videos",
+          localField: "video",
+          foreignField: "_id",
+          as: "video",
+          pipeline: [
+            {
+              $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                  {
+                    $project: {
+                      fullName: 1,
+                      username: 1,
+                      avatar: 1,
+                    },
                   },
-                },
-              ],
+                ],
+              },
             },
-          },
-          {
-            $unwind: "$owner",
-          },
-        ],
+            {
+              $unwind: "$owner",
+            },
+          ],
+        },
       },
-    },
-    {
-      $unwind: "$video",
-    },
-    { $replaceRoot: { newRoot: "$video" } },
-  ]);
+      {
+        $unwind: "$video",
+      },
+      { $replaceRoot: { newRoot: "$video" } },
+    ]);
 
-  if (likedVideos.length < 1) {
-    return response.status(404).json(new ApiError(404, "😰 No video found"));
+    if (likedVideos.length < 1) {
+      return response.status(404).json(new ApiError(404, "😰 No video found"));
+    }
+
+    return response
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          likedVideos,
+          "👍 Liked videos fetched successfully."
+        )
+      );
   }
-
-  return response
-    .status(200)
-    .json(
-      new ApiResponse(200, likedVideos, "👍 Liked videos fetched successfully.")
-    );
-});
+);
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
